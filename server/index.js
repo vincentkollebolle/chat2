91fc57857577c18642849abf0d1c14ced6c2e245
Hadrien Samouillan <hadrien.samouillan@apsys-airbus.com>
2024-06-13 15:05:55 +0200

    update cors server

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http, {
   cors: {
-    origins: ['http://localhost:4200']
+    origins: ['*']
   }
 })
 const users = []
